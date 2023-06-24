a8423de4db0abec3514577aa3eee9195b9ffe813
Fatima <89465115+fatima014@users.noreply.github.com>
2023-06-25 04:55:24 +0500

    Finished Author Routes

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const bodyParser = require('body-parser')
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
 const bookRouter = require('./routes/books');
+const methodOverride = require('method-override')
 
 
 app.set('view engine', 'ejs');
@@ -17,7 +18,8 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
+app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}));
 
 const mongoose  = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
